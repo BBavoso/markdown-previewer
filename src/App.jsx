f8adc44e70911e27045029cbe463a03396c91be1
BBavoso <bmbavoso@gmail.com>
2023-09-07 00:11:25 -0400

    passes all tests

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,24 @@ import "./App.css";
 import Preview from "./Preview";
 
 function App() {
-    const [inputText, setInputText] = useState("");
+    const [inputText, setInputText] = useState(`
+# This is a heading (very large)
+## This is a sub-heading (smaller)
+[This is a link](google.com)
+<br>inline code: \`print("hello")\`
+\`\`\`c
+int main() {
+    printf("hello world);
+}
+\`\`\`
+- this is a list
+- second item in list
+> this is a blockquote (idk)
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+**this text is bold**
+    `);
 
     const onTextEntry = (e) => {
         setInputText(e.target.value);
@@ -11,7 +28,9 @@ function App() {
 
     return (
         <>
-            <textarea id="editor" onChange={onTextEntry}></textarea>
+            <textarea id="editor" onChange={onTextEntry}>
+                {inputText}
+            </textarea>
             <Preview inputText={inputText} />
         </>
     );
--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,12 +1,20 @@
-import { parse } from "marked";
+/* eslint-disable react/prop-types */
+import { marked } from "marked";
 
-// eslint-disable-next-line react/prop-types
+marked.use({ breaks: true });
+
 export default function Preview({ inputText }) {
+    const getParsedText = () => {
+        let parsed = marked.parse(inputText);
+        console.log(parsed);
+        return parsed;
+    };
+
     return (
         <>
             <div
                 id="preview"
-                dangerouslySetInnerHTML={{ __html: parse(inputText) }}
+                dangerouslySetInnerHTML={{ __html: getParsedText() }}
             ></div>
         </>
     );
