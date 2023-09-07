4b77d1f4bd4d43b430dfbe50ed4cf09de376994f
BBavoso <bmbavoso@gmail.com>
2023-09-07 00:11:25 -0400

    passes all tests

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -2,14 +2,12 @@
 import { marked } from "marked";
 
 marked.use({ breaks: true });
+/* eslint-disable react/prop-types */
+import { marked } from "marked";
+
+marked.use({ breaks: true });
 
 export default function Preview({ inputText }) {
-    const getParsedText = () => {
-        let parsed = marked.parse(inputText);
-        console.log(parsed);
-        return parsed;
-    };
-
     return (
         <>
             <div
