9844bb1b326cf7b60f271c0b3a4a1f4fb0bf3edd
BBavoso <bmbavoso@gmail.com>
2023-09-06 23:45:50 -0400

    passes tests 1-4

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,4 +1,13 @@
+import { parse } from "marked";
+
 // eslint-disable-next-line react/prop-types
 export default function Preview({ inputText }) {
-    return <div id="preview">{inputText}</div>;
+    return (
+        <>
+            <div
+                id="preview"
+                dangerouslySetInnerHTML={{ __html: parse(inputText) }}
+            ></div>
+        </>
+    );
 }
